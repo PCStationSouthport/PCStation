841fa2e263d92a5f811178b6f753357e907f241a
PCStationSouthport <pc.station.southport@gmail.com>
2025-03-30 03:49:16 +0100

    Update contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,6 +1,9 @@
     document.getElementById('contact-form').addEventListener('submit', function (event) {
         event.preventDefault(); // Prevent form submission
 
+
+
+
         // Get the reCAPTCHA response token
         var recaptchaResponse = grecaptcha.getResponse();
 
@@ -20,3 +23,10 @@
         // Submit the form
         this.submit();
     });
+
+function onClick(e) {
+    e.preventDefault();
+    grecaptcha.enterprise.ready(async () => {
+      const token = await grecaptcha.enterprise.execute('6LfkzAMrAAAAAEqO5HeaBSBvlNP5GgXGwnX8I8QH', {action: 'LOGIN'});
+    });
+  }
